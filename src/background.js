37f7aa80b256bdedce61a6a5193b04cec04c475d
Robert Spier <robert.spier@gmail.com>
2023-04-23 18:03:28 +0200

    Convert logic to modules

--- src/background.js
+++ src/background.js
@@ -1,22 +1,5 @@
 'use strict';
 
-// With background scripts you can communicate with popup
-// and content files.
-// For more information on background script,
-// See https://developer.chrome.com/extensions/background_pages
-let latestUrl = '';
+import events from './modules/background/events.js';
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  latestUrl = changeInfo.url ? changeInfo.url : latestUrl;
-
-  if (latestUrl && changeInfo.status === 'complete') {
-    chrome.tabs
-      .sendMessage(tabId, {
-        message: 'urlChanged',
-        url: latestUrl,
-      })
-      .catch((error) => {
-        return;
-      });
-  }
-});
+events();
