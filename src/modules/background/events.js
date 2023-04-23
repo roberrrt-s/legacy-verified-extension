6afb63748416b7ee3556db3a37615f4a58e42426
Robert Spier <robert.spier@gmail.com>
2023-04-23 18:03:19 +0200

    Change structure for background to be more modular

---
+++ src/modules/background/events.js
@@ -0,0 +1,22 @@
+export default function events() {
+  function handleTabUpdate() {
+    let latestUrl = '';
+
+    chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+      latestUrl = changeInfo.url ? changeInfo.url : latestUrl;
+
+      if (latestUrl && changeInfo.status === 'complete') {
+        chrome.tabs
+          .sendMessage(tabId, {
+            message: 'urlChanged',
+            url: latestUrl,
+          })
+          .catch((error) => {
+            return;
+          });
+      }
+    });
+  }
+
+  handleTabUpdate();
+}
